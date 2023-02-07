4a783b902ffe40b1bb1b625ccb6f4388c6736c4b
Martin Durant <martin.durant@alumni.utoronto.ca>
2023-02-07 17:04:29 -0500

    Options

--- src/lib.rs
+++ src/lib.rs
@@ -16,12 +16,12 @@ lazy_static! {
 async fn get_url(
     url: &str,
     method: reqwest::Method,
-    head: HashMap<&str, &str>,
+    head: HashMap<&str, String>,
     body: Option<&str>,
 ) -> reqwest::Result<String> {
     let mut req = CLIENT.request(method, url);
     for (key, value) in head.iter() {
-        req = req.header(*key, *value);
+        req = req.header(*key, value);
     }
     req = match body {
         Some(text) => req.body(text.to_string()),
@@ -30,8 +30,14 @@ async fn get_url(
     Ok(req.send().await?.text().await?)
 }
 
-async fn get_url_or(url: &str) -> String {
-    let headers: HashMap<&str, &str> = HashMap::new();
+async fn get_url_or(url: &str, start: usize, end: usize) -> String {
+    let mut headers: HashMap<&str, String> = HashMap::new();
+    if (start > 0) & (end != 0) {
+        headers.insert(
+            "Range".as_ref(),
+            format!("bytes={}-{}", start, end + 1).to_string(),
+        );
+    }
     match get_url(url, reqwest::Method::GET, headers, None).await {
         Ok(text) => text,
         Err(e) => e.to_string(),
@@ -39,17 +45,36 @@ async fn get_url_or(url: &str) -> String {
 }
 
 #[pyfunction]
-fn get_urls(urls: Vec<&str>) -> HashMap<&str, String> {
-    let mut result: HashMap<&str, String> = HashMap::new();
+fn get_ranges(
+    urls: Vec<&str>,
+    starts: Option<Vec<usize>>,
+    ends: Option<Vec<usize>>,
+) -> Vec<String> {
+    let mut result: Vec<String> = Vec::with_capacity(urls.len());
     let coroutine = async {
-        let out = urls.iter().map(|u| get_url_or(*u));
-
-        let bits = join_all(out).await;
-        urls.iter()
-            .zip(bits.iter())
-            .map(|(u, s)| result.insert(*u, s.clone()))
+        match (starts, ends) {
+            (Some(st), Some(en)) => join_all(
+                urls.iter()
+                    .zip(st)
+                    .zip(en)
+                    .map(|((u, s), e)| get_url_or(*u, s, e)),
+            )
+            .await
+            .iter()
+            .map(|s| result.push(s.clone()))
             .into_iter()
-            .count();
+            .count(),
+
+            (None, None) => join_all(urls.iter().map(|u| get_url_or(*u, 0, 0)))
+                .await
+                .iter()
+                .map(|s| result.push(s.clone()))
+                .into_iter()
+                .count(),
+
+            // If you only include starts or only stops, you get no results for now
+            _ => 0,
+        }
     };
     RUNTIME.block_on(coroutine);
     result
@@ -58,6 +83,6 @@ fn get_urls(urls: Vec<&str>) -> HashMap<&str, String> {
 /// A Python module implemented in Rust.
 #[pymodule]
 fn rfsspec(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(get_urls, m)?)?;
+    m.add_function(wrap_pyfunction!(get_ranges, m)?)?;
     Ok(())
 }
