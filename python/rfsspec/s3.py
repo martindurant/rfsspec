e1985ac7cf01f7017b0e3e288830ec5003e2b190
Martin Durant <martin.durant@alumni.utoronto.ca>
2023-04-01 16:07:39 -0400

    for py38

--- python/rfsspec/s3.py
+++ python/rfsspec/s3.py
@@ -1,4 +1,4 @@
-from functools import cache
+from functools import lru_cache
 from rfsspec.rfsspec import s3_cat_ranges, s3_info, s3_find
 
 from fsspec.spec import AbstractFileSystem, AbstractBufferedFile
@@ -69,7 +69,7 @@ class RustyS3File(AbstractBufferedFile):
         return self.fs.cat_file(self.path, start=start, end=end)
 
 
-@cache
+@lru_cache()
 def get_bucket_region(bucket):
     import requests
     return requests.head(f"https://{bucket}.s3.amazonaws.com").headers["x-amz-bucket-region"]
