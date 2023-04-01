8c5791166ffb71eb4b59a6b3e0eaf300677e68f7
Martin Durant <martin.durant@alumni.utoronto.ca>
2023-04-01 15:56:05 -0400

    Update versions; pass through cache options in s3

--- python/rfsspec/__init__.py
+++ python/rfsspec/__init__.py
@@ -3,4 +3,4 @@ from rfsspec.s3 import RustyS3FileSystem
 from rfsspec.gcs import RustyGCSFileSystem
 from rfsspec.azure import RustyAzureFileSystem
 
-__all__ = ["RustyS3FileSystem", "RustyHTTPFileSystem", "RustyGCSFileSystem"]
+__all__ = ["RustyS3FileSystem", "RustyHTTPFileSystem", "RustyGCSFileSystem", "RustyAzureFileSystem"]
